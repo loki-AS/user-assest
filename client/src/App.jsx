19a4fafd80fad95941b13c3e9cf214316f4a9839
loki <lohithgowda6617@gmail.com>
2023-01-06 13:28:21 +0530

    initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import GetUsers from "./components/GetUsers"
 const App = () => {
   return (
     <div className='container'>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/users" element={<GetUsers />} />
       </Routes>
+      
     </div>
   )
 }
